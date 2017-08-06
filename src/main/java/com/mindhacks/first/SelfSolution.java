e3b67b638f5f8ac9f1e1b91688802b6beea96324
MindHacks <zhangwb33@ziroom.com>
2017-08-06 15:59:49 +0800

    first push

---
+++ src/main/java/com/mindhacks/first/SelfSolution.java
@@ -0,0 +1,27 @@
+package com.mindhacks.first;
+
+public class SelfSolution {
+    public int singleNumber(int[] nums) {
+        int findNum=-1;
+        for(int n=0;n<nums.length;n++){
+            if(isSignal(nums[n],nums)){
+                findNum=nums[n];
+            }
+        }
+        return findNum;
+    }
+    
+    boolean isSignal(int testNum,int[] nums){
+        int point=0;
+        for(int n=0;n<nums.length;n++){
+            if(testNum==nums[n]){
+                point++;
+            }
+        }
+        if(point==1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/mindhacks/first/TopSolution.java
@@ -0,0 +1,11 @@
+package com.mindhacks.first;
+
+/**
+ * @Description TODO
+ * @User MindHacks
+ * @Date 2017/8/6 15:57
+ * @Version 1.0.0
+ */
+public class TopSolution {
+
+}
