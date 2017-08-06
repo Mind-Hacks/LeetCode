a2673459cc28d2e7472e3480dccda0e680568990
MindHacks <492643389@qq.com>
2017-08-06 16:13:22 +0800

    提交优秀运行方案

--- src/main/java/com/mindhacks/first/TopSolution.java
+++ src/main/java/com/mindhacks/first/TopSolution.java
@@ -7,5 +7,11 @@ package com.mindhacks.first;
  * @Version 1.0.0
  */
 public class TopSolution {
-
+    public int singleNumber(int[] nums) {
+        int r={0};
+        int n=nums.length;
+        for (int i=0;i!=n;++i)
+            r=r^nums[i];
+        return  r;
+    }
 }
