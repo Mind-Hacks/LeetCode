02812c2b2f8311c36d4d26817aaea3e9df0fba49
MindHacks <zhangwb33@ziroom.com>
2017-08-08 21:15:50 +0800

    整数反转

---
+++ src/main/java/com/mindhacks/Four/ReverseInteger/Solution.java
@@ -0,0 +1,13 @@
+package com.mindhacks.Four.ReverseInteger;
+
+public class Solution {
+    public int reverse(int x) {
+        if (x==0){
+            return 0;
+        }
+        if (x>Integer.MAX_VALUE){
+            return  -1;
+        }
+        return 0;
+    }
+}
