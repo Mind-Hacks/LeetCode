8266f5c4ac49e5759f3fe993bce42abeddd4e17c
MindHacks <zhangwb33@ziroom.com>
2017-08-09 14:32:41 +0800

    创建 栈进行数据反转

---
+++ src/main/java/com/mindhacks/Four/ReverseInteger/ReverseIntegerTest.java
@@ -0,0 +1,22 @@
+package com.mindhacks.Four.ReverseInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description 测试整数反转
+ * @User MindHacks
+ * @Date 2017/8/9 11:41
+ * @Version 1.0.0
+ */
+public class ReverseIntegerTest {
+    private static Logger logger = LoggerFactory.getLogger(ReverseIntegerTest.class);
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        logger.info("将整数反转的结果{}", solution.reverse(123));
+
+        Solution solution1 = new Solution();
+        logger.info("将整数反转的结果{}", solution1.reverse(-123));
+    }
+}
