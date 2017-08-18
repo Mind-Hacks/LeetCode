270dabbc8e45d77c4ea91b8602c2d56b9006637a
MindHacks <492643389@qq.com>
2017-08-18 23:57:51 +0800

    卖东西

---
+++ src/main/java/com/mindhacks/Five/SellStockII/SellStockIITest.java
@@ -0,0 +1,16 @@
+package com.mindhacks.Five.SellStockII;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by MindHacks on 2017/8/12.
+ */
+public class SellStockIITest {
+    private static Logger logger = LoggerFactory.getLogger(SellStockIITest.class);
+
+    public static void main(String[] args) {
+        int[] a=new int[]{1,2,4};
+        System.out.println(new SelfSolution().maxProfit(a));
+    }
+}
