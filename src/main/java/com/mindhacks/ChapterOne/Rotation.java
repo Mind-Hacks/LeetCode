ee51c46534d4c098343e6cc67c27dcf396b68fc4
MindHacks <492643389@qq.com>
2017-08-25 00:00:06 +0800

    字符串反转

---
+++ src/main/java/com/mindhacks/ChapterOne/Rotation.java
@@ -0,0 +1,14 @@
+package com.mindhacks.ChapterOne;
+
+import java.util.*;
+
+public class Rotation {
+    public boolean chkRotation(String A, int lena, String B, int lenb) {
+        if (lena != lenb) {
+            return false;
+        } else {
+            String c= A + A;
+            return c.contains(B);
+        }
+    }
+}
