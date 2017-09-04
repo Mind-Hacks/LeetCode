33075dd30b211e5ff08e8716081fba5a50da0e85
MindHacks <492643389@qq.com>
2017-09-04 08:55:13 +0800

    写了一半的归并排序

--- src/main/java/com/mindhacks/ChapterTwo/MergeSort.java
+++ src/main/java/com/mindhacks/ChapterTwo/MergeSort.java
@@ -1,10 +1,46 @@
 package com.mindhacks.ChapterTwo;
 
-import java.util.*;
 
 public class MergeSort {
     public int[] mergeSort(int[] A, int n) {
-        // write code here
-        return null;
+        if (A == null || n == 0) {
+            return null;
+        }
+        merge(A, 0, A.length - 1);
+        return A;
+    }
+
+    public void merge(int[] A, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int middle = (left + right) / 2;
+        merge(A, left, middle);
+        merge(A, middle + 1, right);
+        compare(A, left, middle, right);
+    }
+
+    public void compare(int[] A, int left, int middle, int right) {
+        int[] tempArray = new int[right - left + 1];
+        int l = left;
+        int r = middle;
+        int index = 0;
+        while (l <= middle && r <= right) {
+            if (A[l] < A[r]) {
+                tempArray[index++] = A[l++];
+            } else {
+                tempArray[index++] = A[r++];
+            }
+        }
+
+        while (l <= left) {
+            tempArray[index] = A[l++];
+        }
+        while (r < right) {
+            tempArray[index] = A[r++];
+        }
+        for (int n = 0; n < tempArray.length; n++) {
+            A[left++] = tempArray[n];
+        }
     }
 }
