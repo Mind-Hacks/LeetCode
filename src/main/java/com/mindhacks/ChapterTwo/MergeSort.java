7b171a06956110c28b669cdc068a240ba86ba378
MindHacks <zhangwb33@ziroom.com>
2017-09-05 10:23:54 +0800

    归并排序调优

--- src/main/java/com/mindhacks/ChapterTwo/MergeSort.java
+++ src/main/java/com/mindhacks/ChapterTwo/MergeSort.java
@@ -1,6 +1,8 @@
 package com.mindhacks.ChapterTwo;
 
-
+/**
+ * 归并排序：可以画成树，然后取值
+ */
 public class MergeSort {
     public int[] mergeSort(int[] A, int n) {
         if (A == null || n == 0) {
@@ -23,7 +25,7 @@ public class MergeSort {
     public void compare(int[] A, int left, int middle, int right) {
         int[] tempArray = new int[right - left + 1];
         int l = left;
-        int r = middle;
+        int r = middle+1;                                           //从middle开始，还是有些小问题
         int index = 0;
         while (l <= middle && r <= right) {
             if (A[l] < A[r]) {
@@ -33,14 +35,23 @@ public class MergeSort {
             }
         }
 
-        while (l <= left) {
-            tempArray[index] = A[l++];
+        while (l <= middle) {
+            tempArray[index++] = A[l++];
         }
-        while (r < right) {
-            tempArray[index] = A[r++];
+        while (r<=right) {
+            tempArray[index++] = A[r++];
         }
         for (int n = 0; n < tempArray.length; n++) {
-            A[left++] = tempArray[n];
+            A[left+n] = tempArray[n];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] A = new int[]{7, 4, 10, 11, 111, 222, 45, 66, 27};
+        int[] B = new MergeSort().mergeSort(A, A.length);
+        for (int n=0;n<B.length;n++){
+            System.out.print(B[n]+" ");
         }
     }
 }
