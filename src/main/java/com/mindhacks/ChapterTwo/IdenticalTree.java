f4ee5c6b7e9e6fb67e494a9a600325b4e01336eb
MindHacks <zhangwb33@ziroom.com>
2017-10-27 13:43:23 +0800

    kmp 算法再次更新

--- src/main/java/com/mindhacks/ChapterTwo/IdenticalTree.java
+++ src/main/java/com/mindhacks/ChapterTwo/IdenticalTree.java
@@ -1,89 +1,64 @@
 package com.mindhacks.ChapterTwo;
 
-import java.util.*;
-
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-
-    public TreeNode(int val) {
-        this.val = val;
-    }
-}
-
 public class IdenticalTree {
-    public boolean chkIdentical(TreeNode t1, TreeNode t2) {
-        String t1Str = serialByPre(t1);
-        String t2Str = serialByPre(t2);
-        return getIndexOf(t1Str, t2Str) != -1;
-    }
-
-    public String serialByPre(TreeNode head) {
-        if (head == null) {
-            return "#!";
-        }
-        String res = head.val + "!";
-        res += serialByPre(head.left);
-        res += serialByPre(head.right);
-        return res;
-    }
-
-    // KMP
-    public int getIndexOf(String s, String m) {
-        if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
-            return -1;
-        }
-        char[] ss = s.toCharArray();
-        char[] ms = m.toCharArray();
-        int[] nextArr = getNextArray(ms);
-        int index = 0;
-        int mi = 0;
-        while (index < ss.length && mi < ms.length) {
-            if (ss[index] == ms[mi]) {
-                index++;
-                mi++;
-            } else if (nextArr[mi] == -1) {
-                index++;
+    public static int KMP(String source, String pattern) {
+        int i = 0, j = 0;
+        char[] src = source.toCharArray();
+        char[] ptn = pattern.toCharArray();
+        int sLen = src.length;
+        int pLen = ptn.length;
+        int[] next = getNextArray(ptn);
+        while (i < sLen && j < pLen) {
+            // 如果j = -1,或者当前字符匹配成功(src[i] = ptn[j]),都让i++,j++
+            if (j == -1 || src[i] == ptn[j]) {
+                i++;
+                j++;
             } else {
-                mi = nextArr[mi];
+                // 如果j!=-1且当前字符匹配失败,则令i不变,j=next[j],即让pattern模式串右移j-next[j]个单位
+                j = next[j];
             }
         }
-        return mi == ms.length ? index - mi : -1;
+        if (j == pLen)
+            return i - j;
+        return -1;
     }
 
 
     public static int[] getNextArray(char[] p) {
-        int[] next = new int[p.length];
-
-        next[0] = -1;
-
-        int j = 0;
-
+        // 已知next[j] = k,利用递归的思想求出next[j+1]的值
+        // 如果已知next[j] = k,如何求出next[j+1]呢?具体算法如下:
+        // 1. 如果p[j] = p[k], 则next[j+1] = next[k] + 1;
+        // 2. 如果p[j] != p[k], 则令k=next[k],如果此时p[j]==p[k],则next[j+1]=k+1,
+        // 如果不相等,则继续递归前缀索引,令 k=next[k],继续判断,直至k=-1(即k=next[0])或者p[j]=p[k]为止
+        int pLen = p.length;
+        int[] next = new int[pLen];
         int k = -1;
-
-        while (j < p.length - 1) {
-
+        int j = 0;
+        next[0] = -1; // next数组中next[0]为-1
+        while (j < pLen - 1) {
             if (k == -1 || p[j] == p[k]) {
-
-                next[++j] = ++k;
-
+                k++;
+                j++;
+                next[j] = k;
             } else {
-
                 k = next[k];
-
             }
-
         }
-
         return next;
-
     }
 
+
     public static void main(String[] args) {
-        String test="762761";
-        int[] nums=getNextArray(test.toCharArray());
-        System.out.println(nums.toString());
+        String str_s = "abcababca";
+        String str_t = "abcabx";
+        //String str_t="caba";
+        int[] nextArray = getNextArray(str_t.toCharArray());
+        System.out.print("{");
+        for (int n = 0; n < nextArray.length; n++) {
+            System.out.print(nextArray[n] + " ");
+        }
+        System.out.print("}");
+        System.out.println(KMP(str_s, str_t));
     }
 
 }
