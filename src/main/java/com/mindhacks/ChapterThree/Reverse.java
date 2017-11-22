e07219ad1391dc253c612502153a580e35c5363c
MindHacks <zhangwb33@ziroom.com>
2017-11-22 23:32:03 +0800

    逆向句子练习

--- src/main/java/com/mindhacks/ChapterThree/Reverse.java
+++ src/main/java/com/mindhacks/ChapterThree/Reverse.java
@@ -4,7 +4,7 @@ package com.mindhacks.ChapterThree;
  * 句子的逆序练习
  */
 public class Reverse {
-    public static  String reverseSentence(String A, int n) {
+    public static String reverseSentence(String A, int n) {
         if (A == null || n == 0) {
             return A;
         }
@@ -13,27 +13,27 @@ public class Reverse {
         return String.valueOf(s);
     }
 
-    public static  void rotateWord(char[] chas) {
+    public static void rotateWord(char[] chas) {
         if (chas == null || chas.length == 0) {
             return;
         }
         reverse(chas, 0, chas.length - 1);
-        int l = -1;
-        int r = -1;
+        int left = -1;
+        int right = -1;
         for (int i = 0; i < chas.length; i++) {
             if (chas[i] != ' ') {
-                l = i == 0 || chas[i - 1] == ' ' ? i : l;
-                r = i == chas.length - 1 || chas[i + 1] == ' ' ? i : r;
+                left = (i == 0 || chas[i - 1] == ' ') ? i : left;
+                right = (i == chas.length - 1 || chas[i + 1] == ' ') ? i : right;
             }
-            if (l != -1 && r != -1) {
-                reverse(chas, l, r);
-                l = -1;
-                r = -1;
+            if (left != -1 && right != -1) {
+                reverse(chas, left, right);
+                left = -1;
+                right = -1;
             }
         }
     }
 
-    public static  void reverse(char[] chas, int start, int end) {
+    public static void reverse(char[] chas, int start, int end) {
         char tmp = 0;
         while (start < end) {
             tmp = chas[start];
@@ -45,8 +45,7 @@ public class Reverse {
     }
 
     public static void main(String[] args) {
-        String str="abc def opf45";
-        reverseSentence(str,str.length());
-        System.out.println(str);
+        String str = "abc def pf45";
+        System.out.println(reverseSentence(str, str.length()));
     }
 }
