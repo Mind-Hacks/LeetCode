1289407df0cc17087dfbf8eae55d69aa8258632e
MindHacks <492643389@qq.com>
2017-12-05 23:23:04 +0800

    无重复子串

--- src/main/java/com/mindhacks/ChapterThree/DistinctSubstring.java
+++ src/main/java/com/mindhacks/ChapterThree/DistinctSubstring.java
@@ -5,9 +5,29 @@ package com.mindhacks.ChapterThree;
  * @user MindHacks
  */
 public class DistinctSubstring {
-    public int longestSubstring(String A, int n) {
-        // write code here
-        char[] charsArray = A.toCharArray();
-        return 0;
+        public static int longestSubstring(String A, int n) {
+            if (A == null || n == 0) {
+                return 0;
+            }
+            char[] chas = A.toCharArray();
+            int[] map = new int[256];
+            for (int i = 0; i < 256; i++) {
+                map[i] = -1;
+            }
+            int len = 0;
+            int pre = -1;
+            int cur = 0;
+            for (int i = 0; i < n; i++) {
+                pre = Math.max(pre, map[chas[i]]);
+                cur = i - pre;
+                len = Math.max(len, cur);
+                map[chas[i]] = i;
+            }
+            return len;
+        }
+
+    public static void main(String[] args) {
+        String testStr="aabcb";
+        System.out.println(longestSubstring(testStr,5));
     }
 }
