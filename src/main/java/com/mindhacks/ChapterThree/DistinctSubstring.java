275bf1e62f5a3ab38645b35d33bea2e483f1f99b
MindHacks <zhangwb33@ziroom.com>
2017-12-05 18:05:30 +0800

    取得无重复字符串

---
+++ src/main/java/com/mindhacks/ChapterThree/DistinctSubstring.java
@@ -0,0 +1,13 @@
+package com.mindhacks.ChapterThree;
+
+/**
+ * @description :返回字符串最长无重复长度
+ * @user MindHacks
+ */
+public class DistinctSubstring {
+    public int longestSubstring(String A, int n) {
+        // write code here
+        char[] charsArray = A.toCharArray();
+        return 0;
+    }
+}
