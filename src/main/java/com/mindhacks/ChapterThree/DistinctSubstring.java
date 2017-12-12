bcc69c4af5cd63af3a528ed5e740174f4d601365
MindHacks <492643389@qq.com>
2017-12-13 01:41:16 +0800

    求字符串最长无重复子串的长度

--- src/main/java/com/mindhacks/ChapterThree/DistinctSubstring.java
+++ src/main/java/com/mindhacks/ChapterThree/DistinctSubstring.java
@@ -1,33 +1,36 @@
 package com.mindhacks.ChapterThree;
 
 /**
- * @description :返回字符串最长无重复长度
- * @user MindHacks
+ * @Description:返回字符串最长无重复长度
+ * @Author:mindHacks
+ * @Date:2017年12月13日1:39
+ * @wiki:
  */
 public class DistinctSubstring {
-        public static int longestSubstring(String A, int n) {
-            if (A == null || n == 0) {
-                return 0;
-            }
-            char[] chas = A.toCharArray();
-            int[] map = new int[256];
-            for (int i = 0; i < 256; i++) {
-                map[i] = -1;
-            }
-            int len = 0;
-            int pre = -1;
-            int cur = 0;
-            for (int i = 0; i < n; i++) {
-                pre = Math.max(pre, map[chas[i]]);
-                cur = i - pre;
-                len = Math.max(len, cur);
-                map[chas[i]] = i;
-            }
-            return len;
+
+    public static int longestSubstring(String A, int n) {
+        if (A == null || n == 0) {
+            return 0;
         }
+        char[] chars = A.toCharArray();
+        int[] maps = new int[256];        //目的是记录下每个每个字符串，之前出现的位置
+        for (int m = 0; m < maps.length; m++) {
+            maps[m] = -1;
+        }
+        int pre = -1;
+        int length = 0;
+        int cur = 0;
+        for (int m = 0; m < chars.length; m++) {
+            pre = Math.max(pre, maps[chars[m]]);
+            cur = m - pre;
+            length = Math.max(cur, length);
+            maps[chars[m]] = m;
+        }
+        return length;
+    }
 
     public static void main(String[] args) {
-        String testStr="aabcb";
-        System.out.println(longestSubstring(testStr,5));
+        String testStr="ABCCECFT";
+        System.out.println(longestSubstring(testStr,testStr.length()));
     }
 }
