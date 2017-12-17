4748aa995d63c7c1fd920c579c88af7031dd39bb
MindHacks <492643389@qq.com>
2017-12-17 23:23:20 +0800

    用栈进行排序

--- src/main/java/com/mindhacks/ChapterFour/TwoStacks.java
+++ src/main/java/com/mindhacks/ChapterFour/TwoStacks.java
@@ -3,25 +3,42 @@ package com.mindhacks.ChapterFour;
 import java.util.ArrayList;
 
 public class TwoStacks {
-    public static int[] twoStacksSort(int[] numbers) {
-        int point=0;
-        getMax(numbers,point);
-        twoStacksSort(numbers);
-        return numbers;
-    }
 
-    public static  int[] getMax(int[] array, int point) {
-        if (point == array.length - 1) {
-            return array;
-        } else {
-            for (int m = point + 1; m < array.length; m++) {
-                if (array[m] > array[point]) {
-                    int temp = array[point];
-                    array[point] = array[m];
-                    array[m] = array[temp];
+    public static ArrayList<Integer> twoStacksSort(int[] numbers) {
+        int len = numbers.length;
+        int[] help = new int[len];
+        int n = len - 1;
+        int m = -1;
+        while (n >= 0) {
+            int key = numbers[n--];
+            if (m == -1) {
+                help[++m] = key;
+            } else {
+                if (help[m] > key) {
+                    help[++m] = key;
+                } else {
+                    int k = m;
+                    while (k >= 0 && help[k] <= key) {
+                        help[k + 1] = help[k];
+                        k--;
+                    }
+                    help[k + 1] = key;
+                    m++;
                 }
             }
-            return null;
+        }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < help.length; i++) {
+            list.add(help[i]);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] test = new int[]{1, 2, 3, 4, 5};
+        ArrayList<Integer> arrayList = twoStacksSort(test);
+        for (Integer array : arrayList) {
+            System.out.print(array);
         }
     }
 }
