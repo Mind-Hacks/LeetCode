21cc63802af2ef8cbae439a563daf27384ffc5cc
MindHacks <zhangwb33@ziroom.com>
2017-12-20 23:49:39 +0800

    链表

---
+++ src/main/java/com/mindhacks/ChapterFive/InsertValue.java
@@ -0,0 +1,26 @@
+package com.mindhacks.ChapterFive;
+
+import java.util.*;
+
+/**
+ * 有一个整数val，如何在节点值有序的环形链表中插入一个节点值为val的节点，并且保证这个环形单链表依然有序。
+ * 给定链表的信息，及元素的值A及对应的nxt指向的元素编号同时给定val，请构造出这个环形链表，并插入该值。
+ * 测试样例：
+ * [1,3,4,5,7],[1,2,3,4,0],2
+ * 返回：{1,2,3,4,5,7}
+ */
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class InsertValue {
+    public ListNode insert(int[] A, int[] nxt, int val) {
+        return null;
+    }
+}
