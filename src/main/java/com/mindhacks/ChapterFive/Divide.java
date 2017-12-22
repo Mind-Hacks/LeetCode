70a1870f470ead6285169cfbf4b62bd369a8ab9e
MindHacks <492643389@qq.com>
2017-12-23 00:15:28 +0800

    链表分段排序

--- src/main/java/com/mindhacks/ChapterFive/Divide.java
+++ src/main/java/com/mindhacks/ChapterFive/Divide.java
@@ -1,16 +1,32 @@
 package com.mindhacks.ChapterFive;
+
 /**
  * 对于一个链表，我们需要用一个特定阈值完成对它的分化，使得小于等于这个值的结点移到前面，大于该值的结点在后面，同时保证两类结点内部的位置关系不变。
- 给定一个链表的头结点head，同时给定阈值val，请返回一个链表，使小于等于它的结点在前，大于等于它的在后，保证结点值不重复。
- 测试样例：
- {1,4,2,5},3
- {1,2,4,5}
+ * 给定一个链表的头结点head，同时给定阈值val，请返回一个链表，使小于等于它的结点在前，大于等于它的在后，保证结点值不重复。
+ * 测试样例：
+ * {1,4,2,5},3
+ * {1,2,4,5}
  */
 
-import java.util.*;
 
 public class Divide {
     public ListNode listDivide(ListNode head, int val) {
-        return  null;
+        if (head == null) {
+            return null;
+        }
+        ListNode current = head;
+        ListNode leftTail = head;
+        ListNode right = head;
+        ListNode rightTail = head;
+        while (current != null) {
+            if (current.val < val) {
+                leftTail.next = current;
+            } else {
+                rightTail.next = current;
+            }
+            current = current.next;
+        }
+        leftTail.next = right;
+        return head;
     }
 }
