58f8caa20e2ebc0eab643e2c2fbfa9ace3c4c481
MindHacks <492643389@qq.com>
2017-12-26 08:28:53 +0800

    清除链表节点元素

---
+++ src/main/java/com/mindhacks/ChapterFive/Palindrome.java
@@ -0,0 +1,17 @@
+package com.mindhacks.ChapterFive;
+/**
+ * 请编写一个函数，检查链表是否为回文。
+ 给定一个链表ListNode* pHead，请返回一个bool，代表链表是否为回文。
+ 测试样例：
+ {1,2,3,2,1}
+ 返回：true
+ */
+
+import com.mindhacks.ListNode;
+
+public class Palindrome {
+    public boolean isPalindrome(ListNode pHead) {
+
+        return true;
+    }
+}
