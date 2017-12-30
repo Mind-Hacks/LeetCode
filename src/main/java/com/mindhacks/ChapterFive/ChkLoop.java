4592b8f7739e9682aaa2c1aac6fb55d2fbeac571
MindHacks <492643389@qq.com>
2017-12-30 22:49:42 +0800

    链表判断是否有环

---
+++ src/main/java/com/mindhacks/ChapterFive/ChkLoop.java
@@ -0,0 +1,12 @@
+package com.mindhacks.ChapterFive;
+
+import com.mindhacks.ListNode;
+
+/**
+ * 链表是否有环形
+ */
+public class ChkLoop {
+    public int chkLoop(ListNode head, int adjust) {
+        return 0;
+    }
+}
