d76fff45bce819c50cad358ca3ef68b3e0ad48ab
MindHacks <492643389@qq.com>
2018-01-03 22:59:21 +0800

    循环有序数组最小值2

--- src/main/java/com/mindhacks/ChapterSix/MinValue.java
+++ src/main/java/com/mindhacks/ChapterSix/MinValue.java
@@ -4,6 +4,18 @@ package com.mindhacks.ChapterSix;
 public class MinValue {
     public int getMin(int[] arr, int n) {
         // write code here
+        if (arr==null || n==0){
+            return  -1;
+        }
+        int left=0;
+        int right=n-1;
+        while (left<=right){
+            int middle=left+(right-left)/2;
+            if (left<=middle){
+                right=middle-1;
+            }
+
+        }
         return 0;
     }
 }
