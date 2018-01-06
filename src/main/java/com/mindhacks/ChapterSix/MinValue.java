ce76729a08d9df73f44bd0e7811c72eacdc070d3
MindHacks <492643389@qq.com>
2018-01-06 15:03:24 +0800

    有序循环数组的定义

--- src/main/java/com/mindhacks/ChapterSix/MinValue.java
+++ src/main/java/com/mindhacks/ChapterSix/MinValue.java
@@ -7,17 +7,29 @@ package com.mindhacks.ChapterSix;
  * <p>
  * 测试样例：
  * [4,1,2,3,3],5
+ 返回：1
+    3 2 1 10 9 8
+ */
+
+/**
+ * 对于一个循环有序数组a1,a2,…,an,存在一个i，
+ * 满足1< i < n,使得a1,a2,…,ai和ai,ai+1,…,an同为单调不减，或单调不增数组。
+ * 且a1,a2,…,ai中的任意一个元素恒大与等于或恒小于等于ai,ai+1,…,an中的任意一个元素。
+ *
+ *
+ * 有序循环数组是指，
+ * 有序数组左边任意长度的部分放到右边去，右边的部分拿到左边来。
+ * 比如数组[1,2,3,3,4]，是有序循环数组
  */
 public class MinValue {
 
-    public int getMin(int[] arr, int n) {
+    public static int getMin(int[] arr,int n) {
         if (arr == null || arr.length == 0) {
             return -1;
         }
         int low = 0;
         int high = arr.length - 1;
         int mid = 0;
-
         while (low < high) {
             if (low == high - 1) {
                 break;
@@ -26,15 +38,10 @@ public class MinValue {
                 return arr[low];
             }
             mid = (low + high) / 2;
-
-
-            //5  1 2 4
             if (arr[low] > arr[mid]) {
                 high = mid;
                 continue;
             }
-
-            //5 6 7 8 9 10 1 4
             if (arr[mid] > arr[high]) {
                 low = mid;
                 continue;
@@ -51,5 +58,12 @@ public class MinValue {
             }
         }
         return Math.min(arr[low], arr[high]);
+    }
+
+
+    public static void main(String[] args) {
+        int[] testArray = new int[]{4,1,2,3,3};
+        int  result=getMin(testArray,testArray.length);
+        System.out.println("结果="+result);
     }
 }
