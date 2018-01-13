691050adb1c644e11a7ff0276391e9810f9bedb7
MindHacks <492643389@qq.com>
2018-01-13 16:36:50 +0800

    叶子节点调整

--- src/main/java/com/mindhacks/Three/SameTree/TreeNode.java
+++ src/main/java/com/mindhacks/Three/SameTree/TreeNode.java
@@ -3,8 +3,8 @@ package com.mindhacks.Three.SameTree;
 //二叉树的叶子节点
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    public TreeNode left;
+    public TreeNode right;
 
     TreeNode(int x) {
         val = x;
