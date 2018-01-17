90f5849d8e026e3da0a0aee75f18f34959787da8
MindHacks <492643389@qq.com>
2018-01-17 23:58:32 +0800

    完全二叉树统计深度4

--- src/main/java/com/mindhacks/ChapterSix/CountNodes.java
+++ src/main/java/com/mindhacks/ChapterSix/CountNodes.java
@@ -44,15 +44,14 @@ public class CountNodes {
     }
 
     public static void main(String[] args) {
+
         TreeNode root = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
         TreeNode treeNode3 = new TreeNode(3);
         TreeNode treeNode4 = new TreeNode(4);
         TreeNode treeNode5 = new TreeNode(5);
-
         root.left=treeNode2;
         root.right=treeNode3;
-
         treeNode2.left=treeNode4;
         treeNode2.right=treeNode5;
         System.out.println(count(root));
