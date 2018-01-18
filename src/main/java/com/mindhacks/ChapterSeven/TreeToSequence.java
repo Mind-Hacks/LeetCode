2a0fdc4a457b6a37d3fc634a601fa65c6529b8cd
MindHacks <492643389@qq.com>
2018-01-18 22:09:05 +0800

    二叉树三种顺序遍历

---
+++ src/main/java/com/mindhacks/ChapterSeven/TreeToSequence.java
@@ -0,0 +1,15 @@
+package com.mindhacks.ChapterSeven;
+
+import com.mindhacks.Three.SameTree.TreeNode;
+
+/**
+ * 请用递归方式实现二叉树的先序、中序和后序的遍历打印。
+
+ 给定一个二叉树的根结点root，请依次返回二叉树的先序，中序和后续遍历(二维数组的形式)。
+ */
+public class TreeToSequence {
+    public int[][] convert(TreeNode root) {
+        // write code here
+        return  null;
+    }
+}
