8a12aeb470688e68eeec13aa108385534515f1b3
MindHacks <zhangwb33@ziroom.com>
2018-03-01 15:26:13 +0800

    二叉树打印练习

--- src/main/java/com/mindhacks/ChapterSeven/TreeToSequence.java
+++ src/main/java/com/mindhacks/ChapterSeven/TreeToSequence.java
@@ -9,8 +9,16 @@ import java.util.List;
  * 请用递归方式实现二叉树的先序、中序和后序的遍历打印。
  * 给定一个二叉树的根结点root，
  * 请依次返回二叉树的先序，中序和后续遍历(二维数组的形式)。
+ *
+ *               1
+ *            /  \
+ *          2     3
+ *        /  \
+ *       4    5
+ *
  */
 public class TreeToSequence {
+
     public static int[][] convert(TreeNode root) {
         if (root == null) {
             return null;
@@ -57,5 +65,21 @@ public class TreeToSequence {
         postOrder(node.left, res);
         postOrder(node.right, res);
         res.add(node.val);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+
+        root.left = treeNode2;
+        root.right = treeNode3;
+        treeNode2.left = treeNode4;
+        treeNode2.right = treeNode5;
+
+        convert(root);
     }
 }
