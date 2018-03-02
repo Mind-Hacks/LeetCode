4f3908d3f7845c3b47b5592c47af35e7080262ff
MindHacks <zhangwb33@ziroom.com>
2018-03-02 14:47:32 +0800

    非递归方式遍历二叉树2

---
+++ src/main/java/com/mindhacks/ChapterSeven/CheckBalance.java
@@ -0,0 +1,15 @@
+package com.mindhacks.ChapterSeven;
+
+
+import com.mindhacks.Three.SameTree.TreeNode;
+
+/**
+ * 有一棵二叉树，请设计一个算法判断这棵二叉树是否为平衡二叉树。
+     给定二叉树的根结点root，请返回一个bool值，
+    代表这棵树是否为平衡二叉树。
+ */
+public class CheckBalance {
+    public boolean check(TreeNode root) {
+        return true;
+    }
+}
--- src/main/java/com/mindhacks/ChapterSeven/TreeToSequenceNew.java
+++ src/main/java/com/mindhacks/ChapterSeven/TreeToSequenceNew.java
@@ -4,114 +4,98 @@ import com.mindhacks.ArrayUtil;
 import com.mindhacks.Three.SameTree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
  * 请用非递归方式实现二叉树的先序、中序和后序的遍历打印。
- 给定一个二叉树的根结点root，请依次返回二叉树的先序，中序和后续遍历(二维数组的形式)。
+ * 给定一个二叉树的根结点root，请依次返回二叉树的先序，中序和后续遍历(二维数组的形式)。
  */
+
 /**
  * @Description:非递归二叉树进行遍历
  * @Author:MindHacks
  * @Date:2018-03-01 16:10:14
- * @wiki:
- *                    1
- *                  /  \
- *                2     3
- *              /  \
- *            4    5
+ * @wiki: 1
+ * /  \
+ * 2     3
+ * /  \
+ * 4    5
  */
 public class TreeToSequenceNew {
 
 
     public static int[][] convert(TreeNode root) {
-        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
-        list.add(preOrder(root));
-        list.add(inOrder(root));
-        list.add(postOrder(root));
-        int[][] res = new int[3][list.get(0).size()];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < list.get(0).size(); j++) {
-                res[i][j] = list.get(i).get(j);
-            }
+        // write code here
+        List<Integer> pre = new ArrayList<Integer>();
+        List<Integer> in = new ArrayList<Integer>();
+        List<Integer> post = new ArrayList<Integer>();
+        preOrder(root, pre);
+        inOrder(root, in);
+        postOrder(root, post);
+        int[][] res = new int[3][pre.size()];
+        for (int i = 0; i < pre.size(); i++) {
+            res[0][i] = pre.get(i);
+            res[1][i] = in.get(i);
+            res[2][i] = post.get(i);
         }
         return res;
     }
 
-    public static ArrayList<Integer> preOrder(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
+    public static void preOrder(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        if (root == null) {
-            return res;
-        }
         stack.push(root);
         while (!stack.isEmpty()) {
-            TreeNode node = stack.pop();
-            res.add(node.val);
-            if (node.right != null) {
-                stack.push(node.right);
-            }
-            if (node.left != null) {
-                stack.push(node.left);
-            }
+            TreeNode cur = stack.pop();
+            list.add(cur.val);
+            if (cur.right != null)
+                stack.push(cur.right);
+            if (cur.left != null)
+                stack.push(cur.left);
         }
-        return res;
     }
 
-    public static ArrayList<Integer> inOrder(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
+    public static void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        while (root != null) {
-            // 依次放入左节点到栈中，并且将其右节点放入stack
-            while (root != null) {
-                if (root.right != null) {
-                    stack.push(root.right);
-                }
-                stack.push(root);
-                root = root.left;
-            }
-            // 访问左节点, 如果没有右节点，将持续访问其父节点
-            root = stack.pop();
-            while (!stack.isEmpty() && root.right == null) {
-                res.add(root.val);
-                root = stack.pop();
-            }
-            // 此时需要加上一个节点
-            res.add(root.val);
-            // 访问下一个节点，左节点父亲的右节点
-            if (!stack.isEmpty()) {
-                root = stack.pop();
+        TreeNode cur = root;
+
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
             } else {
-                root = null;
+                cur = stack.pop();
+                list.add(cur.val);
+                cur = cur.right;
             }
         }
-        return res;
     }
 
-    public static ArrayList<Integer> postOrder(TreeNode root) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
+    public static void postOrder(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode pre = root;
-        while (root != null) {
-            // 左节点依次放入栈中
-            while (root.left != null) {
-                stack.push(root);
-                root = root.left;
+        TreeNode h = root, c = null;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            c = stack.peek();
+            if (c.left != null && (c.left != h && c.right != h)) {
+                stack.push(c.left);
+                c = c.left;
+            } else if (c.right != null && c.right != h) {
+                stack.push(c.right);
+                c = c.right;
+            } else {
+                h = c;
+                TreeNode tmp = stack.pop();
+                list.add(tmp.val);
             }
-            // 右边为空  或者  右边已经遍历过， 遍历root
-            while (root != null && (root.right == null || root.right == pre)) {
-                res.add(root.val);
-                pre = root;
-                if (stack.isEmpty()) {
-                    return res;
-                }
-                root = stack.pop();
-            }
-            // 放入root，先遍历右节点。
-            stack.push(root);
-            root = root.right;
         }
-        return res;
     }
 
 
