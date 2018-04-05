ac3531e59c82823f9687bc89d0cd7ca5dc0e3e80
MindHacks <492643389@qq.com>
2018-04-05 18:56:59 +0800

    斐波那契数列非递归求和

---
+++ src/main/java/com/offer/No9.java
@@ -0,0 +1,35 @@
+package com.offer;
+
+
+/**
+ * @Description: 斐波那契数列
+ * @Author:MindHacks
+ * @Date:2018-04-05 18:41:30
+ * @wiki:
+ */
+public class No9 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public int Fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int preFibonacci = 1;
+        int prePreFibonacci = 0;
+        int result = 0;
+        for (int m = 2; m <= n; m++) {
+            result = preFibonacci + prePreFibonacci;
+            prePreFibonacci = preFibonacci;
+            preFibonacci = result;
+        }
+        return result;
+    }
+
+}
