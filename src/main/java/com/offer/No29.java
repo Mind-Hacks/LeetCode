79d6b70e93a6a10e760af30af57f6522d2aac127
492643389@qq.com <151Zwb22687591>
2018-08-14 14:11:51 +0800

    超过一半元素3

---
+++ src/main/java/com/offer/No29.java
@@ -0,0 +1,17 @@
+package com.offer;
+
+import java.util.ArrayList;
+
+/**
+ * @Description: 输入一个字符串, 按字典序打印出该字符串中字符的所有排列。
+ * 例如输入字符串abc,则打印出由字符a,b,c所，能排列出来的所有字符串abc,acb,bac,bca,cab和cba
+ * @Author:MindHacks
+ * @Date:2018-04-23 10:05:36
+ * @wiki:
+ */
+public class No29 {
+
+    public static  int moreThanHalfnum(int[] nums,int length){
+        return 0;
+    }
+}
