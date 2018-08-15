464967d9c53bc6f878a52d907f2893c8f22ff257
MindHacks <492643389@qq.com>
2018-08-15 23:10:49 +0800

    最小的k个数

---
+++ src/main/java/com/offer/No30.java
@@ -0,0 +1,17 @@
+package com.offer;
+
+import java.util.ArrayList;
+
+/**
+ * @Description:输入n个整数，找出其中最小的K个数。
+ *  例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ * @Author:MindHacks
+ * @Date:2018-08-15 22:58:15
+ * @wiki:  构建最大堆
+ */
+public class No30 {
+
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        return null;
+    }
+}
